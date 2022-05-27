b08e1b7459fd7edd1a0d9fb9c17cf53f0fee172a
Ahmed-Elfeky <elfeky1593@gmail.com>
2022-05-27 19:28:16 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,44 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".dewal").click(function () {
+    $(".dropdown-menu-dewal").slideToggle(300);
+  });
+  $(".mehan").click(function () {
+    $(".dropdown-menu-mehan").slideToggle(300);
+  });
 
-$('.dewal').click(function(){
-    $('.dropdown-menu-dewal').slideToggle(300)
-})
-$('.mehan').click(function(){
-    $('.dropdown-menu-mehan').slideToggle(300)
-})
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+  });
 
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:1,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:2000,
-    autoplayHoverPause:true
-});
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
+  // الجزء الخاص بصفحة تتبع طلبك
 
+  $(".fa-eye").click(function () {
+    $(this).addClass("active");
+    if ($(this).hasClass("active")) {
+      $(this).parent().parent().siblings().slideToggle(800);
+    }
+  });
 
-$('.join').click(function(){
-    $(this).hide()
  
-    $('.form-popup').css('transform','scale(1)')
-})
 
-$('.cancel').click(function(){
-    $('.join').show()
-    $('.form-popup').css('transform','scale(0)')
-})
+  
 
+  // arrow-up in the bottn page button to move up  //////
 
+  $("#arrow-up").click(function () {
+    $("body,html").animate({
+      scrollTop: 0,
+    });
+  });
 
-
-
-
-
-})
+  $(".fa-bars").click(function () {
+    $("#mobile").slideToggle();
+  });
+});
