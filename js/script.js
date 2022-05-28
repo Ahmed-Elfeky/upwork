0d7a2cca20bead5dc52825479a2c6f853112e517
Ahmed-Elfeky <elfeky1593@gmail.com>
2022-05-28 20:12:53 +0200

    fish the project

--- js/script.js
+++ js/script.js
@@ -5,37 +5,29 @@ $(document).ready(function () {
   $(".mehan").click(function () {
     $(".dropdown-menu-mehan").slideToggle(300);
   });
-
+  // slider section///
   var owl = $(".owl-carousel");
   owl.owlCarousel({
     items: 1,
     loop: true,
-    margin: 10,
     autoplay: true,
-    autoplayTimeout: 3000,
+    autoplayTimeout: 2000,
   });
 
   // الجزء الخاص بصفحة تتبع طلبك
-
   $(".fa-eye").click(function () {
     $(this).addClass("active");
     if ($(this).hasClass("active")) {
       $(this).parent().parent().siblings().slideToggle(800);
     }
   });
-
- 
-
-  
-
   // arrow-up in the bottn page button to move up  //////
-
   $("#arrow-up").click(function () {
     $("body,html").animate({
       scrollTop: 0,
     });
   });
-
+  // menu for mobile or small screen////
   $(".fa-bars").click(function () {
     $("#mobile").slideToggle();
   });
